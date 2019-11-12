83471bf8d1ff42693c42e42fa6a5409ba2a55ff4
Tue M. Ngo <tue.ngominh@gmail.com>
2019-11-12 15:37:18 +0100

    Update MainClient.java

--- src/main/java/tech/bts/webdevreview/classes/model/MainClient.java
+++ src/main/java/tech/bts/webdevreview/classes/model/MainClient.java
@@ -2,11 +2,11 @@ package tech.bts.webdevreview.classes.model;
 
 public class MainClient {
     public static void main(String[] args) {
-
+/*
         Client c1 = new Client();
 
         c1.setName("Tue Ngo");
-        c1.setEmail("tue.ngominh@gmail.com");
+        c1.setEmail("tue.ngominh@gmail.com");*/
 
     }
 }
