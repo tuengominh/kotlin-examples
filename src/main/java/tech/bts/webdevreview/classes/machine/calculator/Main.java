ce0263e0cbeb8b12c0cd68b76500788abda9c6f1
Tue M. Ngo <tue.ngominh@gmail.com>
2019-11-12 15:36:27 +0100

    Update Main.java

--- src/main/java/tech/bts/webdevreview/classes/machine/calculator/Main.java
+++ src/main/java/tech/bts/webdevreview/classes/machine/calculator/Main.java
@@ -3,13 +3,13 @@ package tech.bts.webdevreview.classes.machine.calculator;
 public class Main {
     public static void main(String[] args) {
 
-        Calculator calculator = new Calculator(0);
+        /* Calculator calculator = new Calculator(0);
         calculator.add(7);
         calculator.add(8);
 
         calculator.divideBy(3);
 
         System.out.println(calculator.getValue());
-        System.out.println(calculator.getCounter());
+        System.out.println(calculator.getCounter());*/
     }
 }
